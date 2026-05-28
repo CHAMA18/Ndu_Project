4afeda90db40ba460553132d0bde76da87e67f91
Z User <z@container>
2026-05-28 08:57:34 +0000

    Deploy updated settings screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2288968538" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "735278904" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
