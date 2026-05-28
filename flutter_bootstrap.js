c6c3c76e2c5a3c801c81f26f2093346ad9ba5e4b
Z User <z@container>
2026-05-28 12:52:17 +0000

    Deploy: Export PDF working across all pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "514372893" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "8933224" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
